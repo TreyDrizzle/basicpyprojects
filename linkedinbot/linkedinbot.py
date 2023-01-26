a106828680f3c598ca489cc5dea7129fbaafc0fc
TreyDrizzle <113615117+TreyDrizzle@users.noreply.github.com>
2023-01-25 19:03:32 -0600

    Update linkedinbot.py

--- linkedinbot/linkedinbot.py
+++ linkedinbot/linkedinbot.py
@@ -3,8 +3,8 @@ import urllib.parse, random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
 from bs4 import BeautifulSoup
+from urllib.parse import parse_qsl, urljoin, urlparse
 
 def getPeopleLinks(page):
     links = []
@@ -24,6 +24,24 @@ def getJobLinks(page):
                     links.append(url)
     return links
 
+def getID(url):
+    pUrl = urlparse.urlparse(url)
+    return urlparse.parse_qs(pUrl.query)['in']['0']
+
+
+
+def ViewBot(driver):
+    visited = {}
+    pList = []
+    count = 0
+    while True:
+            #sleep to make sure everthing loads.
+            #add random to make us look human. 
+            time.sleep(random.uniform(3.5,6.9))
+            page = BeautifulSoup(driver.page_source)
+            people = getPeopleLinks(page)
+
+
 def Main():
     
     driver = webdriver.Chrome()
@@ -37,6 +55,11 @@ def Main():
     username.send_keys('tcdavissies@gmail.com')
     password.send_keys('linkbot')
     login.click()
+
+    os.system("cls")
+    print; "[+] Success! logged into Linkedin"
+    ViewBot(driver)
+    driver.close()
 
 if __name__ == '__main__':
         Main()
