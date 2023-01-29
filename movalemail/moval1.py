6c3a9bddb5cf54067d86f14c48d136d117ad23fd
TreyDrizzle <113615117+TreyDrizzle@users.noreply.github.com>
2023-01-28 20:21:43 -0600

    Merge branch 'master' of https://github.com/TreyDrizzle/basicpyprojects

--- movalemail/moval1.py
+++ movalemail/moval1.py
@@ -7,13 +7,20 @@ from bs4 import BeautifulSoup
 
 url = "https://www.moval.edu/faculty-staff/"
 
-def email_lookup(url):
+# def email_lookup(url):
+#     url_open =requests.get(url)
+#     soup = BeautifulSoup(url_open._content, 'html.parser')
+#     for link in soup.find_all('a',{'class':'email'}):
+#         mailto = link.get('href')
+#         email = mailto.replace("mailto:", "")
+#         print(email)
+            
+# email_lookup(url)
+
+def find_division(url):
     url_open =requests.get(url)
     soup = BeautifulSoup(url_open._content, 'html.parser')
-    for link in soup.find_all('a',{'class':'email'}):
-        mailto = link.get('href')
-        email = mailto.replace("mailto:", "")
-        print(email)
-            
-email_lookup(url)
+    for link in soup.find_all('tr'):
+        print(link.text)
 
+find_division(url)
