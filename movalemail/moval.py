38d1a0acf1dc0bb14bc9b3262c9ea7c09ed28d97
TreyDrizzle <davistyler.chad@gmail.com>
2023-01-30 10:59:01 -0600

    Update moval.py

--- movalemail/moval.py
+++ movalemail/moval.py
@@ -10,7 +10,6 @@ url = "https://www.moval.edu/faculty-staff/"
 def email_lookup(url):
     url_open =requests.get(url)
     soup = BeautifulSoup(url_open._content, 'html.parser')
-    
     for link in soup.find_all('a',{'class':'email'}):
         print(link.get('href'))
     
